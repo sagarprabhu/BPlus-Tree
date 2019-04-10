539bcc931167736bb80b504b7e327d94831e5962
sagarprabhu <sagarprabhu3@gmail.com>
2019-04-10 19:55:59 -0400

    create node structure for B+ Tree

---
+++ src/Pair.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**class for a key-value dictionary pair
+ * 
+ * @author sagar prabhu
+ *
+ */
+public class Pair {
+	
+	int key;
+	
+	// List of values associated with the key
+	List<String> values;
+	
+	public Pair(int key) {
+		this.key = key;
+		this.values = new ArrayList<String>();
+	}
+	
+	public Pair(int key, String value) {
+		this.key = key;
+		
+		if(this.values == null) 
+			this.values = new ArrayList<String>();
+		
+		this.values.add(value);	
+	}
+	
+	/**
+	 * getter method
+	 * @return values associated with the key
+	 */
+	public List<String> getValues() {
+		return this.values;
+	}
+	
+	/**
+	 * setter method
+	 * @param List of values 
+	 */
+	public void setValues(List<String> values) {
+		this.values = values;
+	}
+
+	public String toString() {
+		return "(" + key +",[" + values + "])";
+	}
+}
---
+++ src/TreeNode.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 
+ * @author sagar prabhu
+ * Class for defining B+ Tree node structure
+ */
+public class TreeNode {
+	/**
+	 * List of dict pairs in the node
+	 */
+	List<Pair> pairs;
+	
+	/**
+	 * List of children of current node
+	 */
+	List<TreeNode> children;
+	
+	/**
+	 * Pointers to previous and next nodes. Set only for leaf nodes. 
+	 */
+	TreeNode prev, next;
+	
+	/**
+	 * Pointer to parent node
+	 */
+	TreeNode parent;
+	
+	/**
+	 * TreeNode Constructor
+	 */
+	public TreeNode() {
+		this.pairs = new ArrayList<>();
+		this.children = new ArrayList<>();
+		this.prev = this.next =  null;
+	}
+	
+	/**
+	 * Getter method for pairs
+	 * @return a list of pairs
+	 */
+	public List<Pair> getPairs(){
+		return pairs;
+	}
+	
+	/**
+	 * Setter method for pairs
+	 * @param pairs List of key-value pairs
+	 */
+	public void setPairs(List<Pair> pairs) {
+		Iterator<Pair> itr = pairs.iterator();
+		while(itr.hasNext()) 
+			this.pairs.add(itr.next());
+	}
+	
+	/**
+	 * Getter method for children
+	 * @return list of children pointers of current node
+	 */
+	public List<TreeNode> getChildren() {
+		return this.children;
+	}
+	
+	public String toString() {
+		return "Pairs= "+this.pairs.toString();
+	}
+	
+}
